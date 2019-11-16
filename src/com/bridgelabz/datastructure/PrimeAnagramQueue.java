ed910b6ade306a574566709097ea320269c93526
amit-kumar-mahato <amitraj0589@gmail.com>
2019-11-16 12:51:45 +0530

    PrimeAnagramQueue done

--- src/com/bridgelabz/datastructure/PrimeAnagramQueue.java
+++ src/com/bridgelabz/datastructure/PrimeAnagramQueue.java
@@ -1,5 +1,22 @@
 package com.bridgelabz.datastructure;
 
-public class PrimeAnagramQueue {
+import com.bridgelabz.util.Utility;
 
+public class PrimeAnagramQueue {
+	public static void main(String[] args) {
+		Utility utility = new Utility();
+		MyQueue<String> myQueue = new MyQueue<>();
+		String[] primeNumbers = utility.prime(1000);
+
+		// prints prime numbers that are anagrams
+		for (int i = 0; i < primeNumbers.length; i++) {
+			for (int j = i + 1; j < primeNumbers.length; j++) {
+				if (utility.anagram(primeNumbers[i], primeNumbers[j])) {
+					myQueue.enqueue(primeNumbers[i]);
+					myQueue.enqueue(primeNumbers[j]);
+				}
+			}
+		}
+		System.out.print(myQueue.display());
+	}
 }
